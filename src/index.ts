1fdd83e015787b28bdd113c9a7d280da9e222576
Dolan Miu <dolan_miu@hotmail.com>
2017-12-10 01:48:29 +0000

    Edit import

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { MaterialGrabber } from "@dolan-bio/tips";
+import { MaterialGrabber } from "@dolanmiu/tips";
 
 module.exports = (context, cb) => {
     this.materialGrabber = new MaterialGrabber();
-    
+
     console.log("Getting tip");
     const tip = this.materialGrabber.grabTip();
-    
+
     cb(null, tip);
 };
